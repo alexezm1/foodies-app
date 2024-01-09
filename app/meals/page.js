bd332c26c44ae1df88600e06a26d492ce04d48db
alexezm1 <alex_speed_96@hotmail.com>
2024-01-08 18:27:17 -0700

    meals page and init meals db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,25 @@
+import MealsGrid from "@/ui/meals/mealsGrid";
+import Link from "next/link";
+import styles from "./page.module.css";
+
 export default function MealsPage() {
-  return <div>Meals Page</div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href={"/meals/share"}>Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
