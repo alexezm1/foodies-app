9ceacb750ffe0a691fccf54f0a40cd04767302d5
alexezm1 <alex_speed_96@hotmail.com>
2024-01-09 19:31:29 -0700

    meal details page

--- app/meals/[mealName]/page.js
+++ app/meals/[mealName]/page.js
@@ -1,3 +1,38 @@
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import styles from "./page.module.css";
+
 export default function MealNamePage({ params }) {
-  return <div>Meal Name: {params.mealName}</div>;
+  const meal = getMeal(params.mealName);
+  if (!meal) {
+    notFound();
+  }
+  const { title, image, summary, instructions, creator, creator_email } = meal;
+
+  const instructionsFormatted = instructions.replace(/\n/g, "<br />");
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={image} alt={summary} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={styles.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: `${instructionsFormatted}`,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
